500d12454efbd2b308554ccea7235c352ab67fe6
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-02-28 15:46:18 +0530

    Rename D.C to f.C

---
+++ f.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+min=a[0];
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+
+}
+for(i=0;i<n;i++)
+{
+
+min=a[i];
+}
+printf("%d",min);
+getch();
+return 0;
+}
