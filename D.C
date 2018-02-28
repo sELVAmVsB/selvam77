d9bb91b6b08af01e506dd8c7341681e8d9b24fd0
sELVAmVsB <35689175+sELVAmVsB@users.noreply.github.com>
2018-02-28 15:45:08 +0530

    Create D.C

---
+++ D.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,min=0,i,a[10];
+min=a[0];
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+if(a[i]<min)
+min=a[i];
+}
+printf("%d",min);
+getch();
+return 0;
+}
